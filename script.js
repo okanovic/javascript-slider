bedee428e91c8e3f462e739ff0f29b41f143e45a
okan.toper <okan.toper@foriba.com>
2019-08-08 13:15:47 +0300

    style.css included

--- script.js
+++ script.js
@@ -5,28 +5,29 @@ let models = [
         link: 'https://www.okantoper.com'
     },
     {
-        name: 'BMW',
-        image: 'image/bmw.jpg',
+        name: 'Honda',
+        image: 'image/honda.jpg',
         link: 'https://www.okantoper.com'
     },
     {
-        name: 'BMW',
-        image: 'image/bmw.jpg',
+        name: 'Mazda',
+        image: 'image/mazda.jpg',
         link: 'https://www.okantoper.com'
     },
     {
-        name: 'BMW',
-        image: 'image/bmw.jpg',
+        name: 'Skoda',
+        image: 'image/skoda.jpg',
         link: 'https://www.okantoper.com'
     },
     {
-        name: 'BMW',
-        image: 'image/bmw.jpg',
+        name: 'Volvo',
+        image: 'image/volvo.jpg',
         link: 'https://www.okantoper.com'
     }
 
 ]
 
 let index = 2;
-
+document.querySelector('.card-title').innerHTML = models[index].name;
 document.querySelector('.card-img-top').setAttribute('src',models[index].image);
+document.querySelector('.card-link').setAttribute('href',models[index].link);
