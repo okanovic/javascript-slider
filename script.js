8153dde2566384b39d21a80242a7a9743e74d329
okan.toper <okan.toper@foriba.com>
2019-08-08 12:35:42 +0300

    img-script uploaded

---
+++ script.js
@@ -0,0 +1,32 @@
+let models = [
+    {
+        name: 'BMW',
+        image: 'image/bmw.jpg',
+        link: 'https://www.okantoper.com'
+    },
+    {
+        name: 'BMW',
+        image: 'image/bmw.jpg',
+        link: 'https://www.okantoper.com'
+    },
+    {
+        name: 'BMW',
+        image: 'image/bmw.jpg',
+        link: 'https://www.okantoper.com'
+    },
+    {
+        name: 'BMW',
+        image: 'image/bmw.jpg',
+        link: 'https://www.okantoper.com'
+    },
+    {
+        name: 'BMW',
+        image: 'image/bmw.jpg',
+        link: 'https://www.okantoper.com'
+    }
+
+]
+
+let index = 2;
+
+document.querySelector('.card-img-top').setAttribute('src',models[index].image);
